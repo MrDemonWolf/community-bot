60b397e794366db47c24ea2143a00dff84844e4a
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2025-01-20 13:43:13 -0600

    Build fail fix?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Hello from{" "}
-            <span className="text-[hsl(280,100%,70%)]">MrDemonWOlf</span>, Inc.
+            Made with ❤️ by
+            <span className="text-[hsl(280,100%,70%)]">MrDemonWolf</span>, Inc.
           </h1>
         </div>
       </main>
